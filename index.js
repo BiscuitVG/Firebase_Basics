176fc41b9aa0494c3810f9e8002d793fe615f401
Biscuit <ixbiscuitmdxd952@gmail.com>
2025-06-10 14:46:06 +0530

    Update Saved Credentials
    >Credentials are saved in const, able to proceed to firebase setup.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// alert("Script connected!");
+const getLoginInfo = function(){
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  console.log(email, password);
+};
+
+document.querySelector(".loginBtn").onclick = function() {
+  getLoginInfo();
+};
